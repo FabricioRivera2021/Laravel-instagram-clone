3aac3298d66fa71b5f17ba3f0d321b77556e92d4
Fabricio Rivera <Fabricio.rivera2012@gmail.com>
2023-01-24 15:35:55 -0300

    pequeños ajustes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
         //configurar fechas en español con carbon
-        Carbon::setUTF8(true);
         Carbon::setLocale('es');
         setlocale(LC_ALL, 'es_MX', 'es', 'ES', 'es_MX.utf8');
     }
