9ee2bdd08bc0ce0e4802e48c04ba72179e641de7
Fabricio Rivera <Fabricio.rivera2012@gmail.com>
2023-01-26 22:54:39 -0300

    maquetando los comentarios y haciendo el ingreso a la BD

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use PhpParser\Node\Expr\New_;
 
 class CommentController extends Controller
 {
@@ -18,7 +21,22 @@ class CommentController extends Controller
             'content' => 'string|required'
         ]);
 
+        //Recoger los datos
+        $user = Auth::user();
         $image_id = $request->input('image_id');
         $content = $request->input('content');
+
+        //asignar los valores al nuevo objeto de comentario a guardar en la BD
+        $comment = New Comment();
+        $comment->user_id = $user->id;
+        $comment->image_id = $image_id;
+        $comment->content = $content;
+
+        $comment->save();//guardar en la DB
+
+        return redirect()->route('image.detail', ['id' => $image_id])
+                         ->with([
+                            'status' => "Has ingresado un comentario con exito"
+                         ]);
     }
 }
--- resources/views/images/detail.blade.php
+++ resources/views/images/detail.blade.php
@@ -6,7 +6,6 @@
             <div class="col-md-10">
                     <div class="card pub_image">
                         <div class="card-header">
-                            
                             <div class="container-avatar">
                                 @if ($image->user->image)
                                 <img class="avatar" src="{{ route('user.avatar', ['filename' => $image->user->image]) }}"
@@ -18,16 +17,13 @@
                                 {{ $image->user->name . ' ' . $image->user->surname }}
                                 <span class="nickname">{{ ' - @'.$image->user->nick }}</span>
                             </div>
-
                         </div>
 
                         <div class="card-body">
                             <div class="image-container">
                                 <img class="post-img" src="{{ route('image.file', ['filename'=>$image->image_path]) }}" alt="post-img">
                             </div>
-                            <div class="likes">
 
-                            </div>
                             <div class="description">
                                 <p>{{$image->description}}</p>
                             </div>
@@ -38,6 +34,18 @@
                             </div>
 
                             <div class="comment-container">
+
+                                <div class="show-comments">
+                                    @foreach($image->comments as $comment)
+                                    <div class="detail_comments">
+                                        <div class="detail_comment">
+                                            {{ $comment->content }}
+                                        </div>
+                                        <span class="nickname">{{ ' - @'.$comment->user->nick }} {{ $comment->created_at->diffForHumans() }}</span>
+                                    </div>
+                                    @endforeach
+                                </div>
+
                                 <form action="{{ route('comment.save') }}" method="POST">
                                     @csrf
                                     <input type="hidden" name="image_id" value="{{$image->id}}" />
@@ -48,6 +56,11 @@
                                         <div class="text-danger">
                                             <p>{{ $errors->first('content') }}</p>
                                         </div>
+                                    @endif
+                                    @if(session('status'))
+                                    <div class="alert alert-success">
+                                        {{ session('status') }}
+                                    </div>
                                     @endif
                                     <button type="submit" class="btn btn-comments btn-success">Enviar</button>
                                 </form>
